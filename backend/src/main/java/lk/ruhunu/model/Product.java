4f45abaaac94d2e26a4d5e817767d64d35d83370
Chesintha <139671816+Chesintha@users.noreply.github.com>
2025-06-27 16:11:40 +0530

    Update Product.java

--- backend/src/main/java/lk/ruhunu/model/Product.java
+++ backend/src/main/java/lk/ruhunu/model/Product.java
@@ -1,5 +1,32 @@
 package lk.ruhunu.model;
 
-public class Product {
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String image;
+
+    @Column(name = "new_price", nullable = false)
+    private Double newPrice;
+
+    @Column(name = "old_price", nullable = false)
+    private Double oldPrice;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date = new Date();
+
+    private Boolean available = true;
 }
