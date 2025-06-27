f9e15f299fa6de997c5b165a1e1e7ec42f9a9b3d
Chesintha <139671816+Chesintha@users.noreply.github.com>
2025-06-27 16:11:10 +0530

    Update User.java

--- backend/src/main/java/lk/ruhunu/model/User.java
+++ backend/src/main/java/lk/ruhunu/model/User.java
@@ -1,5 +1,30 @@
 package lk.ruhunu.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> cartItems = new ArrayList<>();
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date = new Date();
 }
